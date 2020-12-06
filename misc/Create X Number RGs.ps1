f123cf5e5ace63890f03044e018b67b64593a6c9
claytonfuselier <53104388+claytonfuselier@users.noreply.github.com>
2020-12-05 19:33:52 -0600

    Create Create X Number RGs.ps1
    Also created section for misc mini scripts

---
+++ misc/Create X Number RGs.ps1
@@ -0,0 +1,18 @@
+﻿# Creates x number of randomly named resource groups.
+# Useful for testing various scenarios or automations.
+
+
+$x = 5 # Number of RGs to create
+$location = "centralus"
+
+$i=0
+while ($i -lt $x) {
+    $num = Get-Random -Maximum 1000
+    $name = "test-$num"
+
+    $new = New-AzResourceGroup -Name $name -Location $location
+    if ($new) {
+        Write-Host "Created $name"
+		$i++
+    }
+}
